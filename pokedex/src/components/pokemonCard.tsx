3ca2cdedfefd841b9982a8a9ec3436b93732bc42
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-20 12:23:02 +0100

    add object

--- pokedex/src/components/pokemonCard.tsx
+++ pokedex/src/components/pokemonCard.tsx
@@ -1,10 +1,19 @@
-export default function PokemonCard(){
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>
-            Bulbasaur
-            </figcaption>
-        </figure>
-    )
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+export default function PokemonCard() {
+  return pokemonList.map((pokemon) => (
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  ));
 }
