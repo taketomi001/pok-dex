22c29ead9583c82053637b79c48b19986921d40c
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-20 11:36:06 +0100

    bilbizar

---
+++ pokedex/src/components/pokemonCard.tsx
@@ -0,0 +1,10 @@
+export default function PokemonCard(){
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+            <figcaption>
+            Bulbasaur
+            </figcaption>
+        </figure>
+    )
+}
