9482d92863d5f63cea42ad5d6ae1e68649b72757
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-21 16:34:16 +0100

    finish

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { useState } from "react";
 // import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/pokemonCard";
+
 const pokemonList = [
   {
     name: "bulbasaur",
@@ -8,17 +10,61 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const nextPokemon = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+
+  const prevPokemon = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const pokemon = pokemonList[pokemonIndex];
+
   return (
     <section>
       <div>
-      {pokemonList.map((pokemon, index) => (
-        <PokemonCard key={index} name={pokemon.name} imgSrc={pokemon.imgSrc} />))}
+        <PokemonCard
+          // key={pokemon.name}
+          name={pokemon.name}
+          imgSrc={pokemon.imgSrc}
+        />
       </div>
+      {pokemonIndex > 0 && (
+        <button onClick={prevPokemon} className="prev">
+          precedent
+        </button>
+      )}
+
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={nextPokemon}>next</button>
+      )}
+    
     </section>
   );
 }
