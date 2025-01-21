e531bc3027c14b4df60b0e22e35ac236d564b573
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-21 13:50:03 +0100

    change code

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -2,10 +2,14 @@ import "./App.css";
 // import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/pokemonCard";
 
+
+
 function App() {
   return (
     <section>
      <div><PokemonCard/></div>
+     
+     
     </section>
   );
 }
--- pokedex/src/components/pokemonCard.tsx
+++ pokedex/src/components/pokemonCard.tsx
@@ -9,11 +9,17 @@ const pokemonList = [
   },
 ];
 
+// const pokemon = { name: "mew", imgSrc: null };
 export default function PokemonCard() {
-  return pokemonList.map((pokemon) => (
-    <figure>
-      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
-      <figcaption>{pokemon.name}</figcaption>
-    </figure>
-  ));
+  const pokemon = pokemonList[1];
+  console.log(pokemon);
+  
+  return (
+    <>
+      <figure>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </>
+  );
 }
